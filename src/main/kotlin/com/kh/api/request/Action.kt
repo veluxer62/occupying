be3865b91e1981144ebb12ae799078e765db4dcd
남경호 <anytimedebug@tripstore.kr>
2019-10-06 17:12:29 +0900

    refactor: clientExtra 데이터 타입을 변경한다

--- src/main/kotlin/com/kh/api/request/Action.kt
+++ src/main/kotlin/com/kh/api/request/Action.kt
@@ -2,7 +2,7 @@ package com.kh.api.request
 
 data class Action(
         val name: String,
-        val clientExtra: Any?,
+        val clientExtra: Map<String, Any>?,
         val params: Params,
         val id: String,
         val detailParams: Map<String, DetailParam>
