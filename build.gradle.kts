2f918bbfa7af338cdda0223b735673e90d36a30e
남경호 <anytimedebug@tripstore.kr>
2019-10-16 16:38:14 +0900

    feat: E-Mail 라이브러리 추가
    E-Mail 발송을 위해 의존성을 추가한다

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
