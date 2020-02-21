b84b7bac0c5c6c892eef7a5c5dcdfad0bc2ce1f5
남경호 <anytimedebug@tripstore.kr>
2020-02-21 11:25:47 +0900

    refactor: gradle 버전관리 구조 변경
    gralde.property파일을 생성하고
    버전관리에 대한 책임을 해당 파일을 가지도록 변경한다.
    
    issue items: #3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "occupying"
+
+pluginManagement {
+    plugins {
+        repositories {
+            gradlePluginPortal()
+        }
+        val kotlinJvmVersion: String by extra
+        val springBootVersion: String by extra
+        val springDependencyManagementVersion: String by extra
+
+        kotlin("jvm") version kotlinJvmVersion
+        kotlin("plugin.spring") version kotlinJvmVersion
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyManagementVersion
+    }
+}
