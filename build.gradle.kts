29735b5ce7271f2a2dcfddc362158ef26f1bd8d1
남경호 <anytimedebug@tripstore.kr>
2020-02-21 11:25:47 +0900

    refactor: gradle 버전관리 구조 변경
    gralde.property파일을 생성하고
    버전관리에 대한 책임을 해당 파일을 가지도록 변경한다.
    
    issue items: #3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val jvmTargetVersion: String by project
+val projectVersion: String by project
+val snakeyamlVersion: String by project
+val mockitoInlineVersion: String by project
+
 plugins {
-	id("org.springframework.boot") version "2.1.8.RELEASE"
-	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	kotlin("jvm") version "1.2.71"
-	kotlin("plugin.spring") version "1.2.71"
+	id("org.springframework.boot")
+	id("io.spring.dependency-management")
+	kotlin("jvm")
+	kotlin("plugin.spring")
 }
 
 group = "com.kh"
-version = "0.4.0"
+version = projectVersion
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
@@ -21,21 +26,21 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.yaml:snakeyaml:1.25")
+	implementation("org.yaml:snakeyaml:$snakeyamlVersion")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude("junit", "junit")
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.junit.jupiter:junit-jupiter-api")
 	testImplementation("org.junit.jupiter:junit-jupiter-params")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-	testRuntimeOnly("org.mockito:mockito-inline:3.1.0")
+	testRuntimeOnly("org.mockito:mockito-inline:$mockitoInlineVersion")
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+		jvmTarget = jvmTargetVersion
 	}
 }
 
