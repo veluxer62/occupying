a4dad2eeb4d9b66287554fe688565f9db98a57c7
남경호 <veluxer62@gmail.com>
2020-02-28 11:25:32 +0900

    Root path router 추가
    Health check를 위한 Root path router를 추가한다.
    
    issue items: #15

---
+++ src/test/kotlin/com/kh/api/HomeApiTest.kt
@@ -0,0 +1,20 @@
+package com.kh.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.test.web.reactive.server.WebTestClient
+
+internal class HomeApiTest {
+
+    private val route = Router().homeRoute()
+    private val client = WebTestClient.bindToRouterFunction(route).build()
+
+    @Test
+    fun `root path will return response ok`() {
+        client.get()
+                .uri("/")
+                .exchange()
+                .expectStatus()
+                .isOk
+    }
+
+}
